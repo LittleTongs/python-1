87c574e329df5defec57a39869cd60628cdc2fab
LittleTongs <148984331+LittleTongs@users.noreply.github.com>
2023-11-06 13:00:28 +0800

    Add files via upload

---
+++ python作业.py
@@ -0,0 +1,30 @@
+# Lv1
+"""a = 0
+b = 0
+a  = int (input())
+b  = int (input())
+c = a + b
+print(c)
+"""
+# Lv2
+a = int(input())
+if a > 0:
+    b = str(a)
+    c = b[::-1]
+    d = int(c)
+    print(d)
+if a < 0:
+    b = abs(a)
+    c = str(b)
+    d = c[::-1]
+    e = int(d)
+    f = str(e)
+    print("-"+f)
+
+
+
+
+
+
+
+
